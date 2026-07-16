579473d3e5d36a0b981caac54ea1f4ae766deb1a
Aditya khapre <khapreaditya11@gmail.com>
2026-07-16 14:29:22 +0530

    Fix build errors and sync Supabase migrations

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -45,7 +45,7 @@ function Button({
   variant = "default",
   size = "default",
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: React.ComponentPropsWithoutRef<"button"> & VariantProps<typeof buttonVariants>) {
   return (
     <ButtonPrimitive
       data-slot="button"
---
+++ types/base-ui.d.ts
@@ -0,0 +1,1 @@
+declare module '@base-ui/react/button';
